cc26bab5fef960032e79470551dc45362bbe0dbf
stan-tikhonov <52580542+stan-tikhonov@users.noreply.github.com>
2019-10-14 20:06:13 +0300

    Add files via upload

---
+++ mapping_script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#1) Prepare output directories
+mkdir star_output
+mkdir count_result_star
+
+#2) run STAR and FeatureCounts for every sample
+cd fastq.trim
+for var1 in *_1.fastq.gz
+do
+  var2="$(echo $var1 | sed -e "s/_1.fastq.gz/_2.fastq.gz/g")"
+  outcount="$(echo $var1 | sed -e "s/_1.fastq.gz/.count/g")"
+  dirname="$(echo $var1 | sed -e "s/_1.fastq.gz//g")"
+  mkdir ../star_output/$dirname
+  STAR --runThreadN 10 --genomeDir ../mouse-annotation-gtf-file --readFilesIn $var1 $var2 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ../star_output/$dirname/
+  featureCounts -F GTF -a ../mouse-annotation-gtf-file/all_mouse.gtf -g gene_id -t exon -o ../count_result_star/$outcount -s 0 -p -C -B -T 10 ../star_output/$dirname/Aligned.sortedByCoord.out.bam
+done
