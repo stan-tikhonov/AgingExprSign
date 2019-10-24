ad70b2170db1c070c967fc5abc6c1e58c5adb235
stan-tikhonov <52580542+stan-tikhonov@users.noreply.github.com>
2019-10-24 21:03:46 +0300

    Update mapping_script.sh

--- mapping_script.sh
+++ mapping_script.sh
@@ -11,6 +11,6 @@ do
   outcount="$(echo $var1 | sed -e "s/_1.fastq.gz/.count/g")"
   dirname="$(echo $var1 | sed -e "s/_1.fastq.gz//g")"
   mkdir ../star_output/$dirname
-  STAR --runThreadN 10 --genomeDir ../mouse-annotation-gtf-file --readFilesIn $var1 $var2 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ../star_output/$dirname/
-  featureCounts -F GTF -a ../mouse-annotation-gtf-file/all_mouse.gtf -g gene_id -t exon -o ../count_result_star/$outcount -s 0 -p -C -B -T 10 ../star_output/$dirname/Aligned.sortedByCoord.out.bam
+  STAR --runThreadN 10 --genomeDir ~/genomes/mus_musculus/star_indexed --readFilesIn $var1 $var2 --readFilesCommand zcat --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ../star_output/$dirname/
+  featureCounts -F GTF -a ~/genomes/mus_musculus/gtf/Mus_musculus.GRCm38.98.gtf -g gene_id -t exon -o ../count_result_star/$outcount -s 0 -p -C -B -T 10 ../star_output/$dirname/Aligned.sortedByCoord.out.bam
 done
